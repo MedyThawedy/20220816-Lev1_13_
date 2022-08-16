c6478a4c2508af443274d6c80dc87012ec81e769
MD <dhmedy@googlemail.com>
2022-08-16 11:41:10 +0200

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+
+let meinText1 = ["Hello", "World"];
+let meinText2 = ["Anass", "Elaine", "Eric", "Georg"];
+let meinText3 = ["Superman", "Wonderwoman", "Hulk", "Batman", "Spiderman"];
+
+/* First test */
+
+let join1 = meinText1.join();
+console.log(join1);
+let join2 = meinText1.join("");
+console.log(join2);
+let join3 = meinText1.join(" ");
+console.log(join3);
+let join4 = meinText1.join("+");
+console.log(join4);
+
+/* Second test */
+
+let join5 = meinText2.join();
+console.log(join5);
+let join6 = meinText2.join("");
+console.log(join6);
+let join7 = meinText2.join(" ");
+console.log(join7);
+let join8 = meinText2.join("+");
+console.log(join8);
+
+/* Third test*/
+
+let join9 = meinText3.join();
+console.log(join9);
+let join10 = meinText3.join("");
+console.log(join10);
+let join11 = meinText3.join(" ");
+console.log(join11);
+let join12 = meinText3.join("+");
+console.log(join12);
